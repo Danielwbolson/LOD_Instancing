b6a3f05216dfcca5f322cb137f7dab15b0ab767c
Seth Johnson <joh08230@umn.edu>
2018-02-01 16:58:06 -0600

    Added a hello-world string in VTK plugin to demonstrate plugin linking

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Text;
+using System;
+
+
+
+public class HelloWorld : MonoBehaviour {
+	[DllImport("vtkplugin", EntryPoint = "answer")] unsafe private static extern int answer();
+	[DllImport("vtkplugin", EntryPoint = "hello_world")] unsafe private static extern IntPtr hello_world();
+	[DllImport("vtkplugin", EntryPoint = "free_data")] unsafe private static extern void free_data(IntPtr p);
+
+	// Use this for initialization
+	void Start () {
+		print ("Hello! The answer from VTK is " + answer ());
+		unsafe{
+			IntPtr str = hello_world ();
+			string s = Marshal.PtrToStringAnsi (str);
+
+			print ("Hello! The greeting from VTK is '" + s + "'");
+
+			free_data (str);
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
