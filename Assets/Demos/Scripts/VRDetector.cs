b04dda1a59c2f59d2d7653912c8979f74a8cd92e
Jung Nam <namxx054@umn.edu>
2018-02-02 11:14:43 -0600

    Rearranged Folder Structure and added VR scene pieces

--- Assets/Demos/Scripts/VRDetector.cs
+++ Assets/Demos/Scripts/VRDetector.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 using UnityEngine.XR;
 
 public class VRDetector : MonoBehaviour {
-
+    public GameObject VR_Camera;
+    public GameObject non_VR_Camera;
 	// Use this for initialization
 	void Start () {
 		if (XRDevice.isPresent) {
 			print ("Virtual Reality device present");
-		} else {
+            VR_Camera.SetActive(true);
+            non_VR_Camera.SetActive(false);
+        } else {
 			print ("Virtual Reality device not present");
-			gameObject.SetActive (false);
+            VR_Camera.SetActive (false);
+            non_VR_Camera.SetActive(true);
 
 		}
 	}
