3569455bb0cd150c4c8e4af683eca44a63f656d1
Seth Johnson <joh08230@umn.edu>
2018-02-02 09:29:21 -0600

    Tried adding a check for VR device enabled

---
+++ Assets/VRDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (XRDevice.isPresent) {
+			print ("Virtual Reality device present");
+		} else {
+			print ("Virtual Reality device not present");
+			gameObject.SetActive (false);
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
