3ff4d2ca64c1c756da9fea9db055d53b24623ad4
Seth Johnson <joh08230@umn.edu>
2018-02-02 14:32:43 -0600

    Added data iso surface rendering, fixed some bugs, texture some things,  and added temporary slider for adjusting iso amount

---
+++ Assets/VTK/Scripts/VTKContour.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+
+public class VTKContour : MonoBehaviour {
+	public VTKData vtkData;
+	public float isovalue;
+
+	private IntPtr NULL = IntPtr.Zero;
+
+	[DllImport("vtkplugin")] unsafe private static extern int contour (void *d, float v, int *np, float **p, float **n, int *nt, int **t);
+	[DllImport("vtkplugin")] unsafe private static extern void free_data (void* h);
+
+	// Use this for initialization
+	void Start () {
+		print ("starting");
+		isovalue = 0.6f;
+		vtkContour ();
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SetIsoValue(float f)
+	{
+		isovalue = f;
+		vtkContour ();
+	}
+
+
+	unsafe public void vtkContour()
+	{
+		if (vtkData == null)
+			vtkData = gameObject.transform.parent.gameObject.GetComponent<VTKData> ();
+		if (vtkData.handle != null) 
+		{
+
+			print ("Hello");
+			int np = 0;
+			int nt = 0;
+			float* p = null;
+			float* n = null;
+			int *t = null;
+			print ((int)(void*)vtkData.handle);
+
+			contour ((void*)vtkData.handle, isovalue, &np, &p, &n, &nt, &t);
+
+			int[] triangles = new int[nt*3];
+			Vector3[] vertices = new Vector3[np];
+			Vector3[] normals = new Vector3[np];
+
+			for (int i = 0; i < np; i++) {
+				float n1 = (float)n [i * 3 + 0];
+				float n2 = (float)n [i * 3 + 1];
+				float n3 = (float)n [i * 3 + 2];
+
+				float v1 = (float)p [i * 3 + 0];
+				float v2 = (float)p [i * 3 + 1];
+				float v3 = (float)p [i * 3 + 2];
+
+				vertices [i].x = v1;
+				vertices [i].y = v2;
+				vertices [i].z = v3;
+
+				normals [i].x = n1;
+				normals [i].y = n2;
+				normals [i].z = n3;
+
+			}
+
+			int num_t = nt*3;
+			for (int i = 0; i < num_t; i++) {
+				triangles [i] = t [i];
+			}
+
+			free_data ((void*)p);
+			free_data ((void*)n);
+			free_data ((void*)t);
+			p = null;
+			n = null;
+			t = null;
+			Mesh mesh = new Mesh ();
+			GetComponent<MeshFilter> ().mesh = mesh;
+			mesh.vertices = vertices;
+			mesh.triangles = triangles;
+			mesh.normals = normals;
+			mesh.RecalculateNormals ();
+			//
+			//print ("YY " + isovalue + " " + np + " " + nt );
+		} else
+			print ("not!");
+	}
+}
---
+++ Assets/VTK/Scripts/VTKData.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+using System.Text;
+using System;
+using System.Linq;
+
+
+
+public class VTKData : MonoBehaviour {
+	private IntPtr NULL = IntPtr.Zero;
+	public string filename;
+	unsafe public void* handle = null;
+
+	[DllImport("vtkplugin", EntryPoint = "answer")]  private static extern int answer();
+	[DllImport("vtkplugin", EntryPoint = "open_data")]  unsafe private static extern void* open_data (StringBuilder sb);
+	[DllImport("vtkplugin", EntryPoint = "close_data")]  unsafe private static extern void   close_data (void* h);
+	[DllImport("vtkplugin", EntryPoint = "get_number_of_vertices")]  unsafe private static extern int   get_number_of_vertices (void* h);
+	[DllImport("vtkplugin", EntryPoint = "get_bounds")]  unsafe private static extern IntPtr get_bounds (void* h);
+
+
+
+	private Vector3 data_center;
+	private Vector3 data_size;
+
+	void Start () {
+		filename = Application.dataPath + "/data/test.vti";
+		LoadData( );
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SetFileName(string f)
+	{
+		filename = f;
+	}
+
+	public void OnDrawGizmos()
+	{
+		unsafe{
+			if (handle != null) {
+				print ("GIZMO");
+
+				Gizmos.color = Color.yellow;
+
+			
+				//print (center.x + ", " + size.x);
+				Gizmos.matrix = transform.localToWorldMatrix;
+				print (data_size);
+				Gizmos.DrawWireCube (data_center, data_size);
+			}
+		}
+	}
+
+	public void LoadData()
+	{
+		print(answer());
+		print ("Loading " + filename);
+
+		unsafe{
+			if (handle != null)
+				close_data (handle);
+			// char* s = ;
+			print("opening..");
+			handle = open_data (new StringBuilder(filename));
+
+			if (handle == null)
+				print ("Could not open " + filename);
+			else
+				print("Opened " + filename);
+
+
+
+			IntPtr bbox_buff;
+			double[] bbox = new double[6];
+
+			bbox_buff = get_bounds (handle);
+			Marshal.Copy (bbox_buff, bbox, 0, 6);
+			Vector3 mm = new Vector3 ((float)(bbox [0]), (float)(bbox [2]), (float)(bbox [4]));
+			Vector3 MM = new Vector3 ((float)(bbox [1]), (float)(bbox [3]), (float)(bbox [5]));
+
+			Vector3 center = (mm + MM) / 2;
+			Vector3 size = MM - mm;
+			float[] sizes = { size.x, size.y, size.z };
+			float extremum = sizes.Max ();
+			gameObject.transform.localScale = new Vector3(1f/extremum,1f/extremum,1f/extremum);
+			data_size = size;
+			data_center = center;
+			//print ("loaded " + get_number_of_vertices (handle) + " vertices");
+		}
+	}
+
+	public int GetNumberOfVertices()
+	{
+		unsafe {
+			if (handle != null)
+				return get_number_of_vertices (handle);
+			else
+				return -1;
+		}
+	}
+}
