3f2496f98eebfeab489566e57915b32f76ff2cce
Seth Johnson <joh08230@umn.edu>
2018-02-22 08:47:49 -0600

    Drawing instanced cylinders on streamline points

---
+++ Assets/Demos/Materials/SplineFollower.shader
@@ -0,0 +1,236 @@
+﻿Shader "Instanced/SplineFollower" 
+{
+	Properties{
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_NormTex("Normal",2D) = "white" {}
+		_offset("Offset",float) = 0
+
+	}
+		SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 200
+
+		CGPROGRAM
+		// Physically based Standard lighting model
+#pragma surface surf Standard addshadow vertex:vert
+#pragma multi_compile_instancing
+#pragma instancing_options procedural:setup
+
+		sampler2D _MainTex;
+		sampler2D _NormTex;
+
+	struct Input {
+		float2 uv_MainTex;
+	};
+
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+	//StructuredBuffer<float4> positionBuffer;
+	StructuredBuffer<float4> _positions;
+	StructuredBuffer<int> _offsets;
+
+#else
+	float4 _positions[1];
+#endif
+
+	float4x4 _DataTransform;
+
+	int _numPositions;
+	int _numLines;
+	float _offset;
+	float _meshHeight;
+
+	float _glyphScale = 1;
+	float _glyphRadius = 1;
+
+	float _glyphInflate=0;
+
+	float _glyphSpacing = 0;
+	float _glyphTwist = 0;
+	float _glyphTextureScale = 1;
+
+	void rotate2D(inout float2 v, float r)
+	{
+		float s, c;
+		sincos(r, s, c);
+		v = float2(v.x * c - v.y * s, v.x * s + v.y * c);
+	}
+
+	void setup()
+	{
+
+	}
+	float3 GetCatmullRomPosition(float t, float3 p0, float3 p1, float3 p2, float3 p3)
+	{
+		
+		//The coefficients of the cubic polynomial (except the 0.5f * which I added later for performance)
+		float3 a = 2 * p1;
+		float3 b = p2 - p0;
+		float3 c = 2 * p0 - 5 * p1 + 4 * p2 - p3;
+		float3 d = -p0 + 3 * p1 - 3 * p2 + p3;
+
+		//The cubic polynomial: a + b * t + c * t^2 + d * t^3
+		float3 pos = 0.5 * (a + (b * t) + (c * t * t) + (d * t * t * t));
+
+		return pos;
+	}
+
+	float3 GetCatmullRomDerivative(float t, float3 p0, float3 p1, float3 p2, float3 p3)
+	{
+		//The coefficients of the cubic polynomial (except the 0.5f * which I added later for performance)
+		float3 a = 2 * p1;
+		float3 b = p2 - p0;
+		float3 c = 2 * p0 - 5 * p1 + 4 * p2 - p3;
+		float3 d = -p0 + 3 * p1 - 3 * p2 + p3;
+
+		//The cubic polynomial: a + b * t + c * t^2 + d * t^3
+		float3 der = 0.5 * b + t*(c+1.5*d*t);
+
+
+		return der;
+	}
+
+
+
+	int4 getSegmentIndices(int seg) {
+		int4 result;
+		int index0 = seg==0? 0 : seg - 1;
+		int index1 = seg;
+		int index2 = seg + 1;
+		int index3 = seg + 2 <= _numPositions - 1? seg + 2: seg + 1;
+
+		result.x = index0;
+		result.y = index1;
+		result.z = index2;
+		result.w = index3;
+
+		return result;
+	}
+
+	float3 getSplinePosition(float t) {
+		int seg = floor (t);
+
+		float seg_t = t - seg;
+		if (seg > _numPositions - 2) {
+			seg = _numPositions - 2;
+			seg_t = 1;
+		} 
+		if(seg < 0) {
+			seg = 0;
+			seg_t = 0;
+		}
+		int4 index = getSegmentIndices (seg);
+		float3 p0 = _positions[index.x];
+		float3 p1 = _positions[index.y];
+		float3 p2 = _positions[index.z];
+		float3 p3 = _positions[index.w];
+		return (GetCatmullRomPosition (seg_t, p0.xyz, p1.xyz, p2.xyz, p3.xyz));
+	}
+
+	float3 getSplineDerivative(float t) {
+		int seg = floor (t);
+
+		float seg_t = t - seg;
+		if (seg > _numPositions - 2) {
+			seg = _numPositions - 2 ;
+			seg_t = 1;
+		}
+		if(seg < 0) {
+			seg = 0;
+			seg_t = 0;
+		}
+		int4 index = getSegmentIndices (seg);
+		float3 p0 = _positions[index.x];
+		float3 p1 = _positions[index.y];
+		float3 p2 = _positions[index.z];
+		float3 p3 = _positions[index.w];
+		return (GetCatmullRomDerivative (seg_t, p0.xyz, p1.xyz, p2.xyz, p3.xyz));
+	}
+
+
+	float4x4 rotationAroundAxis(float3 u, float a){
+		float4x4 transform;
+		float c = cos(a);
+		float s = sin(a);
+		float nc = 1-c;
+
+		transform[0] = float4(c+u.x*u.x*nc,u.x*u.y*nc-u.z*s,u.x*u.z*nc+u.y*s,0);
+		transform[1] = float4(u.y*u.x*nc+u.z*s,c+u.y*u.y*nc,u.y*u.z*nc-u.x*s,0);
+		transform[2] = float4(u.z*u.x*nc-u.y*s,u.z*u.y*nc+u.x*s,c+u.z*u.z*nc,0);
+		transform[3] = float4(0,0,0,1);
+		return (transform);
+
+	}
+	 void vert (inout appdata_full v) {
+	 	#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+	 		 //v.vertex.x += unity_InstanceID;
+	 		 v.vertex.w = 1;
+	 		float z = v.vertex.y*1/(_meshHeight*2)*_glyphScale;
+	 		float centerT = (unity_InstanceID  + _offset)*(_glyphScale+_glyphSpacing);
+
+	 		float instT = centerT +z;
+	 		if(false) {
+			float _RotationSpeed = _glyphTwist*instT;
+			float sinX = sin ( _RotationSpeed  );
+			float cosX = cos ( _RotationSpeed  );
+			float sinY = sin ( _RotationSpeed  );
+			float2x2 rotationMatrix = float2x2( cosX, -sinX, sinY, cosX);          
+
+			v.vertex.xz = mul(rotationMatrix,v.vertex.xz);
+ 
+			v.normal.xz = mul(rotationMatrix,v.normal.xz);}
+
+
+
+//	 		float3 pointA = getSplinePosition(unity_InstanceID+_offset);;
+//	 		float3 pointB = getSplinePosition(unity_InstanceID+1+_offset);;
+			v.vertex.y *= 0.25;
+			v.vertex.xyz *= 0.25;
+
+	 		float3 ABVector;// = normalize(pointB.xyz - pointA.xyz);
+	 		ABVector = normalize(getSplineDerivative(centerT +z));
+	 		float3 splinePos = getSplinePosition(centerT  + z);
+	 		float3 crossVector = normalize(cross(float3(0,1,0),ABVector));
+	 		float d = dot(float3(0,1,0),ABVector);
+
+	 		float angle = acos(d);
+
+	 		//v.vertex.xyz*=1;
+
+
+
+	 		//v.vertex.xz *= 1/(1.0*2)*_glyphScale*_glyphRadius;
+
+	 		//v.vertex.xyz = mul(rotationAroundAxis(crossVector,angle),v.vertex).xyz;
+	 		//v.normal.xyz = mul(rotationAroundAxis(crossVector,angle),v.normal).xyz;
+
+	 		//v.vertex.xyz += _glyphInflate*v.normal.xyz;
+	 		v.vertex.xyz+=_positions[unity_InstanceID];
+
+	 		v.vertex.xyz = mul(_DataTransform,v.vertex).xyz;
+	 			 									 		 return;
+
+	 		return;
+
+          	v.vertex.xyz += float3(splinePos.x,splinePos.y,splinePos.z);
+
+          	v.texcoord.y  = instT*pow(2,_glyphTextureScale);
+        #endif
+
+
+      }
+	void surf(Input IN, inout SurfaceOutputStandard o) 
+	{
+		float4 col = 1.0f;
+
+
+		col = float4(1, 1, 1, 1);
+
+		fixed4 c = tex2D(_MainTex, IN.uv_MainTex.yx) * col;
+		o.Albedo = c;
+		o.Normal.xyz = tex2D(_NormTex,IN.uv_MainTex.yx).xyz;
+		o.Alpha = c.a;
+	}
+	ENDCG
+	}
+		FallBack "Diffuse"
+}
--- Assets/VTK/Scripts/StreamlineRenderer.cs
+++ Assets/VTK/Scripts/StreamlineRenderer.cs
@@ -7,10 +7,14 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Linq;
 
+
+
 public class StreamlineRenderer : Filter {
 	public VTKData vtkData;
 	public float isovalue = 0.001f;
 	bool is_valid = false;
+
+	protected List<Spline> splines;
 
 	[DllImport("vtkplugin")] unsafe private static extern int contour (void *d, float v, int *np, float **p, float **n, int *nt, int **t);
 	[DllImport("vtkplugin")] unsafe private static extern void free_data (void* h);
@@ -30,20 +34,29 @@ public class StreamlineRenderer : Filter {
 	private Vector3 [] line_normals = null;
 	private float[] line_values = null;
 
+	private List< List<Vector4> > uniformSplines;
+
 	unsafe void OnDrawGizmos() {
 		if (!is_valid)
 			return;
 		Gizmos.matrix = transform.localToWorldMatrix;
 		UnityEngine.Random.InitState (2);
 
-		for (int l = 0; l < ids.Count; l++) {
+		for (int l = 0; l < uniformSplines.Count; l++) {
 			Gizmos.color =  UnityEngine.Random.ColorHSV();
-			print (ids [l].Count);
 
-			for (int i = 1; i < ids [l].Count; i++) {
-				Vector3 A = line_positions [ids [l][(i - 1)]];
-				Vector3 B = line_positions [ids[l][(i)]];
+			for (int i = 1; i < uniformSplines [l].Count; i++) {
+				Vector3 A = uniformSplines [l] [(i - 1)];
+				Vector3 B = uniformSplines [l] [(i)];
 
+				if (i == 1)
+					Gizmos.color = Color.green;
+				else if (i == uniformSplines [l].Count - 1)
+					Gizmos.color = Color.red;
+				else if (i % 2 == 0)
+					Gizmos.color = Color.white;
+				else
+					Gizmos.color = Color.black;
 				Gizmos.DrawLine(A,B)  ;
 			}
 		}
@@ -62,6 +75,7 @@ public class StreamlineRenderer : Filter {
 		{
 			
 
+			splines = new List<Spline> ();
 
 			float* normals = null;
 			float* dataValues = null;
@@ -102,10 +116,22 @@ public class StreamlineRenderer : Filter {
 				}
 				free_data(id_list);
 			}
-			print (num_elements_normals);
-			print (num_components_normals);
-			print (normals [0] + "," + normals [1] + "," + normals [2]);
-			print (num_components_data);
+
+			uniformSplines = new List<List<Vector4> >() ;
+			//print (ids.Count + " == " + number_of_lines);
+			for (int l = 0; l < ids.Count; l++) {
+				splines.Add (new Spline ());
+				
+				for (int i = 0; i < ids [l].Count; i++) {
+					splines.Last ().addControlPoint (new Vector4(line_positions[ids [l] [i]].x,line_positions[ids [l] [i]].y,line_positions[ids [l] [i]].z,1));
+				}
+				//print (splines.Last().getLength(0, splines.Last ().controlPointCount ()-1));
+				uniformSplines.Add (splines.Last ().generateUniformPositions (1f));
+			}
+			//print (num_elements_normals);
+			//print (num_components_normals);
+			//print (normals [0] + "," + normals [1] + "," + normals [2]);
+			//print (num_components_data);
 
 			print (dataValues [0]);
 
@@ -113,8 +139,430 @@ public class StreamlineRenderer : Filter {
 			free_data (dataValues);
 			free_data (points);
 			is_valid = true;
+			UpdateBuffer ();
 
 		} //else
 		//print ("not!");
+	}
+
+	private ComputeBuffer positionBuffer;
+	private ComputeBuffer offsetBuffer;
+	public Material _material;
+	public Mesh _mesh;
+
+	void UpdateBuffer() {
+		List<int> offsets = new List<int> ();
+		List<Vector4> points = new List<Vector4> ();
+
+		int offset = 0;
+		for (int i = 0; i < uniformSplines.Count; i++) {
+			offsets.Add (offset);
+			offset += uniformSplines[i].Count;
+			for (int p = 0; p < uniformSplines [i].Count; p++) {
+				//points.Add (new Vector4 (i, p, 0, 1));
+				//print (points.Last ());
+				points.Add (uniformSplines [i] [p]);
+			}
+		}
+
+		positionBuffer = new ComputeBuffer (points.Count,sizeof(float)*4);
+		positionBuffer.SetData (points.ToArray());
+
+		offsetBuffer = new ComputeBuffer (offsets.Count, sizeof(int));
+		offsetBuffer.SetData (offsets.ToArray ());
+
+
+		if (_material != null) {
+			_material.SetBuffer ("_positions", positionBuffer);
+			Debug.Log ("SET BUFFER");
+			_material.SetInt ("_numPositions", points.Count);
+
+			_material.SetBuffer ("_offsets", offsetBuffer);
+			Debug.Log ("SET BUFFER");
+			_material.SetInt ("_numLines", offsets.Count);
+
+		}
+	}
+
+	private int cachedInstanceCount = -1;
+	private ComputeBuffer argsBuffer;
+
+	public float _offset;
+	public float _glyphScale = 1;
+	public float _glyphRadius = 1;
+	public float _glyphInflate = 0;
+	public float _glyphTwist = 0;
+	public float _glyphSpacing = 0;
+	public float _glyphTextureScale = 0;
+	private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+	public int instanceCount = 3;
+
+
+	void UpdateBuffers()
+	{ 
+		if ( instanceCount < 1 ) instanceCount = 1;
+
+		uint numIndices = (_mesh != null) ? (uint)_mesh.GetIndexCount(0) : 0;
+		args[0] = numIndices;
+		args[1] = (uint)instanceCount;
+		argsBuffer.SetData(args);
+
+		cachedInstanceCount = instanceCount;
+	}
+
+
+	void Start() {
+
+		_material.SetFloat("_meshHeight",_mesh.bounds.extents.y);
+		print ("_meshHeight " +  _mesh.bounds.extents.y);
+
+		argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+		UpdateBuffers();
+
+	}
+	void Update()
+	{ 
+		if (!is_valid)
+			return;
+		_material.SetMatrix ("_DataTransform", transform.localToWorldMatrix);
+		_material.SetFloat("_offset", _offset);
+		_material.SetFloat ("_glyphScale", _glyphScale);
+		_material.SetFloat ("_glyphRadius", _glyphRadius);
+		_material.SetFloat ("_glyphInflate", _glyphInflate);
+		_material.SetFloat ("_glyphTwist", _glyphTwist);
+		_material.SetFloat ("_glyphSpacing", _glyphSpacing);
+		_material.SetFloat ("_glyphTextureScale", _glyphTextureScale);
+
+		// Update starting position buffer
+		if (cachedInstanceCount != instanceCount) UpdateBuffers();
+
+		// Pad input
+		//if (Input.GetAxisRaw("Horizontal") != 0.0f) instanceCount = (int)Mathf.Clamp(instanceCount + Input.GetAxis("Horizontal") * 40000, 1.0f, 5000000.0f);
+
+		// Render
+		Graphics.DrawMeshInstancedIndirect(_mesh, 0, _material, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), argsBuffer,0,null, UnityEngine.Rendering.ShadowCastingMode.Off);
+		//  instanceMaterial.SetBuffer("positionBuffer", positionBuffer);
+	}
+
+
+}
+
+
+
+
+
+
+public class Spline
+{
+
+	public List<Vector3> controlPointsList;
+
+	public Spline() {
+		controlPointsList = new List<Vector3>();
+	}
+
+	public void addControlPoint(Vector3 t, float e = 0.001f) {
+		//if(controlPointsList.Count != 0)
+			//Debug.Log ((controlPointsList.Last () - t).magnitude + " < " + e);
+		if(controlPointsList.Count == 0 || (controlPointsList.Last() - t).magnitude > e)
+			controlPointsList.Add (t);
+
+	}
+
+	public int controlPointCount() {
+		return controlPointsList.Count;
+	}
+	public int[] getSegmentIndices(int seg) {
+		int[] result = new int[4];
+		int index0 = seg==0? 0 : seg - 1;
+		int index1 = seg;
+		int index2 = seg + 1;
+		int index3 = seg + 2 <= controlPointCount() - 1? seg + 2: seg + 1;
+
+		result [0] = index0;
+		result [1] = index1;
+		result [2] = index2;
+		result [3] = index3;
+
+		return result;
+	}
+	public float getSegmentLength(int seg) {
+
+
+		int[] index = getSegmentIndices (seg);
+		Vector3 p0 = controlPointsList[index[0]];
+		Vector3 p1 = controlPointsList[index[1]];
+		Vector3 p2 = controlPointsList[index[2]];
+		Vector3 p3 = controlPointsList[index[3]];
+
+		float len = GetLengthSimpsons (0, 1, p0, p1, p2, p3);
+		return len;
+	}
+
+	public float getLength(float t_start, float t_end) {
+		//			float t_i_a = t_start;
+		//			float t_i_b = Mathf.Floor (t_start) + 1;
+		//Debug.Log(t_start +" -- " + t_end);
+		float t_i = t_start;
+
+		float len = 0;
+
+		while (t_i <= t_end) {
+			int seg = (int)Mathf.Floor (t_i );
+
+			float seg_t = t_i - seg;
+
+			float t_j = Mathf.Min (t_end, seg + 1f)-seg;
+			if (seg < controlPointCount () - 1) {
+				int[] index = getSegmentIndices (seg);
+				Vector3 p0 = controlPointsList [index [0]];
+				Vector3 p1 = controlPointsList [index [1]];
+				Vector3 p2 = controlPointsList [index [2]];
+				Vector3 p3 = controlPointsList [index [3]];
+
+
+
+				len += GetLengthSimpsons (seg_t, t_j, p0, p1, p2, p3);
+			}
+
+			t_i += 1;
+			//Debug.Log (t_i);
+
+		}
+
+
+		return len;
+
+
+
+	}
+
+	public Vector3 getSplinePosition(float t) {
+		int seg = (int)Mathf.Floor (t);
+
+		float seg_t = t - seg;
+		if (seg > controlPointCount () - 2) {
+			seg = controlPointCount () - 2;
+			seg_t = 1;
+		}
+		int[] index = getSegmentIndices (seg);
+		Vector3 p0 = controlPointsList[index[0]];
+		Vector3 p1 = controlPointsList[index[1]];
+		Vector3 p2 = controlPointsList[index[2]];
+		Vector3 p3 = controlPointsList[index[3]];
+		return (GetCatmullRomPosition (seg_t, p0, p1, p2, p3));
+	}
+
+	public Vector3 getSplineDerivative(float t) {
+		int seg = (int)Mathf.Floor (t);
+
+		float seg_t = t - seg;
+		if (seg > controlPointCount () - 2) {
+			seg = controlPointCount () - 2 ;
+			seg_t = 1;
+		}
+		int[] index = getSegmentIndices (seg);
+		Vector3 p0 = controlPointsList[index[0]];
+		Vector3 p1 = controlPointsList[index[1]];
+		Vector3 p2 = controlPointsList[index[2]];
+		Vector3 p3 = controlPointsList[index[3]];
+		return (GetCatmullRomDerivative (seg_t, p0, p1, p2, p3));
+	}
+
+
+	//Use Newton–Raphsons method to find the t value at the end of this distance d
+	public float FindTValue(float d)
+	{
+		d = Mathf.Max (0, d);
+		int segment = 0;
+		float segmentLength = getLength (segment, segment + 1);
+		while (d > segmentLength && segment < controlPointCount()-1) {
+			d -= segmentLength;
+
+			segment++;
+			segmentLength = getLength (segment, segment + 1);
+		}
+		if (segment == controlPointCount()-1)
+			return segment;
+		int[] index = getSegmentIndices (segment);
+		Vector3 p0 = controlPointsList[index[0]];
+		Vector3 p1 = controlPointsList[index[1]];
+		Vector3 p2 = controlPointsList[index[2]];
+		Vector3 p3 = controlPointsList[index[3]];
+
+		//Need a start value to make the method start
+		//Should obviously be between 0 and 1
+		//We can say that a good starting point is the percentage of distance traveled
+		//If this start value is not working you can use the Bisection Method to find a start value
+		//https://en.wikipedia.org/wiki/Bisection_method
+		float t = d / segmentLength;
+
+		//Need an error so we know when to stop the iteration
+		float error = 0.001f;
+
+		//We also need to avoid infinite loops
+		int iterations = 0;
+
+		while (true)
+		{
+			//Newton's method
+			float tNext = t - ((GetLengthSimpsons(0f, t, p0, p1,p2,p3) - d) / GetArcLengthIntegrand(t, p0, p1,p2,p3));
+
+			//Have we reached the desired accuracy?
+			if (Mathf.Abs(tNext - t) < error)
+			{
+				break;
+			}
+
+			t = tNext;
+
+			iterations += 1;
+
+			if (iterations > 1000)
+			{
+				break;
+			}
+		}
+		return segment + t;
+	}
+
+
+
+
+	//Returns a position between 4 Vector3 with Catmull-Rom spline algorithm
+	//http://www.iquilezles.org/www/articles/minispline/minispline.htm
+	Vector3 GetCatmullRomPosition(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+	{
+
+		//The coefficients of the cubic polynomial (except the 0.5f * which I added later for performance)
+		Vector3 a = 2f * p1;
+		Vector3 b = p2 - p0;
+		Vector3 c = 2f * p0 - 5f * p1 + 4f * p2 - p3;
+		Vector3 d = -p0 + 3f * p1 - 3f * p2 + p3;
+
+		//The cubic polynomial: a + b * t + c * t^2 + d * t^3
+		Vector3 pos = 0.5f * (a + (b * t) + (c * t * t) + (d * t * t * t));
+
+		return pos;
+	}
+
+	Vector3 GetCatmullRomDerivative(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+	{
+		//The coefficients of the cubic polynomial (except the 0.5f * which I added later for performance)
+		Vector3 a = 2f * p1;
+		Vector3 b = p2 - p0;
+		Vector3 c = 2f * p0 - 5f * p1 + 4f * p2 - p3;
+		Vector3 d = -p0 + 3f * p1 - 3f * p2 + p3;
+
+		//The cubic polynomial: a + b * t + c * t^2 + d * t^3
+		Vector3 der = 0.5f * b + t*(c+1.5f*d*t);
+
+
+		return der;
+	}
+
+
+
+
+
+
+
+
+	//The derivative of cubic De Casteljau's Algorithm
+	Vector3 DeCasteljausAlgorithmDerivative(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+	{
+
+		Vector3 A = 2f * p1;
+		Vector3 B = p2 - p0;
+		Vector3 C = 2f * p0 - 5f * p1 + 4f * p2 - p3;
+		Vector3 D = -p0 + 3f * p1 - 3f * p2 + p3;
+
+		Vector3 dU = t * t * (-3f * (A - 3f * (B - C) - D));
+
+		dU += t * (6f * (A - 2f * B + C));
+
+		dU += -3f * (A - B); 
+
+		return dU;
+	}
+
+
+	//Get and infinite small length from the derivative of the curve at position t
+	float GetArcLengthIntegrand(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+	{
+		//The derivative at this point (the velocity vector)
+		Vector3 dPos = GetCatmullRomDerivative(t, p0, p1,p2,p3);
+
+		//This the how it looks like in the YouTube videos
+		//float xx = dPos.x * dPos.x;
+		//float yy = dPos.y * dPos.y;
+		//float zz = dPos.z * dPos.z;
+
+		//float integrand = Mathf.Sqrt(xx + yy + zz);
+
+		//Same as above
+		float integrand = dPos.magnitude;
+
+		return integrand;
+	}
+
+
+	//Get the length of the curve between two t values with Simpson's rule
+	float GetLengthSimpsons(float tStart, float tEnd, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+	{
+		//This is the resolution and has to be even
+		int n = 20;
+
+		//Now we need to divide the curve into sections
+		float delta = (tEnd - tStart) / (float)n;
+
+		//The main loop to calculate the length
+
+		//Everything multiplied by 1
+		float endPoints = GetArcLengthIntegrand(tStart, p0, p1,p2,p3) + GetArcLengthIntegrand(tEnd, p0, p1,p2,p3);
+
+		//Everything multiplied by 4
+		float x4 = 0f;
+		for (int i = 1; i < n; i += 2)
+		{
+			float t = tStart + delta * i;
+
+			x4 += GetArcLengthIntegrand(t, p0, p1,p2,p3);
+		}
+
+		//Everything multiplied by 2
+		float x2 = 0f;
+		for (int i = 2; i < n; i += 2)
+		{
+			float t = tStart + delta * i;
+
+			x2 += GetArcLengthIntegrand(t, p0, p1,p2,p3);
+		}
+
+		//The final length
+		float length = (delta / 3f) * (endPoints + 4f* x4 + 2f * x2);
+
+		return length;
+	}
+
+
+	public List<Vector4> generateUniformPositions(float stepSize) {
+		List<Vector4> result = new List<Vector4> ();
+
+		float t = 0;
+		float dist = 0;
+		float nextT = 0;
+
+		Vector3 p = getSplinePosition (t);
+		result.Add (new Vector4(p.x, p.y, p.z,1));
+
+		while(t<controlPointCount()-1){
+			dist += stepSize;
+			nextT = FindTValue (dist);
+			t = nextT;
+			 p = getSplinePosition (t);
+			result.Add (new Vector4(p.x, p.y, p.z,1));
+		}
+		return result;
 	}
 }
