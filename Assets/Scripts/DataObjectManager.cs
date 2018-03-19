3c4f6f742aada19b899e518b1a81e31dbc0e722c
Seth Johnson <joh08230@umn.edu>
2018-03-19 15:53:46 -0500

    Moved file searching to a DataObjectManager game object

---
+++ Assets/Scripts/DataObjectManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataObjectManager : MonoBehaviour {
+
+	public string path;
+
+	public List<string> filePaths;
+
+	// Use this for initialization
+	void Start () {
+		Refresh ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Refresh() {
+		print ("Searching " + Application.streamingAssetsPath + "/" + "example_data/VTK" + "/"); 
+		//if (path.Length !=0) {
+			SearchDirectory (Application.streamingAssetsPath + "/" + "example_data/VTK" + "/");
+		//}
+
+		print (filePaths.Count + " vtk files found");
+	}
+	void SearchDirectory(string path) {
+		if (filePaths == null)
+			filePaths = new List<string> ();
+		else
+			filePaths.Clear ();
+		try{
+			foreach (string file in System.IO.Directory.GetFiles(path))
+			{ 	
+				if (!file.EndsWith (".meta")) {
+					filePaths.Add (file);
+
+				}
+			}
+		} catch(DirectoryNotFoundException e) {
+			print("Could not find path " +path);
+		}
+	}
+
+
+}
