72163b298fa906ca7707d53b58e7661e2c971c31
Seth Johnson <joh08230@umn.edu>
2018-03-19 14:11:36 -0500

    Scroll buttons now generated for all VTK files

---
+++ Assets/Scripts/FilePickerButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+public class FilePickerButton : MonoBehaviour {
+
+	public Button button;
+	public Text nameLabel;
+	public Text descLabel;
+	public Image iconImage;
+
+	private string dataFilePath;
+	private FileScrollList scrollList = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Setup(string currentDataFile, FileScrollList currentScrollList) {
+		dataFilePath = currentDataFile;
+		nameLabel.text = Path.GetFileName(currentDataFile);
+		descLabel.text = Path.GetExtension (currentDataFile);
+		//iconImage.sprite = dataFile.icon;
+
+		scrollList = currentScrollList;
+
+	}
+
+}
---
+++ Assets/Scripts/FileScrollList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class DataFile {
+	public string fileName;
+	public Sprite icon;
+	public string details = "";
+}
+
+
+public class FileScrollList : MonoBehaviour {
+
+	//public List<DataFile> dataFileList;
+	public Transform contentPanel; 
+
+	public SimpleObjectPool buttonObjectPool;
+
+	public DataFileList dataFileList;
+	// Use this for initialization
+	void Start () {
+		RefreshDisplay ();
+	}
+
+	private void RefreshDisplay() {
+		AddButtons ();
+	}
+	private void AddButtons() {
+		for (int i = 0; i < dataFileList.paths.Count; i++) {
+			print (i);
+			string file = dataFileList.paths [i];
+			GameObject newButton = buttonObjectPool.GetObject ();
+			newButton.transform.SetParent (contentPanel, false);
+			FilePickerButton button = newButton.GetComponent<FilePickerButton> ();
+			button.Setup (file, this);
+		}
+	}
+}
