8799d6204c2082fd869dba94c97f3712357081e5
Danielwbolson <danielwbolson@gmail.com>
2018-05-30 12:31:41 -0500

    Working on the LOD instancing - Scrapped the bounding box system for my own based on some magic number math - New system should be faster, however hard to tell since nothing draws - Running into some issues of null computebuffers because that LOD level is empty

--- Assets/Shaders/DemoShader.shader
+++ Assets/Shaders/DemoShader.shader
@@ -13,7 +13,7 @@
         CGPROGRAM
         // Physically based Standard lighting model
 #pragma surface surf Standard addshadow fullforwardshadows
-#pragma multi_compile_instancing
+#pragma multi_compile_instancing LOD0 LOD1 LOD2 LOD3
 #pragma instancing_options procedural:setup
 
         sampler2D _MainTex;
@@ -30,6 +30,10 @@
 
 #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
         StructuredBuffer<ObjInfo> dataBuffer;
+        StructuredBuffer<ObjInfo> LOD0Buffer;
+        StructuredBuffer<ObjInfo> LOD1Buffer;
+        StructuredBuffer<ObjInfo> LOD2Buffer;
+        StructuredBuffer<ObjInfo> LOD3Buffer;
 #else
         float4 color;
 #endif
@@ -40,16 +44,44 @@
 
         void setup() {
 #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
-            pos = dataBuffer[unity_InstanceID].position;
-
+#ifdef LOD0
+            pos = LOD0Buffer[unity_InstnceID].position;
             unity_ObjectToWorld._11_21_31_41 = float4(pos.w, 0, 0, 0) *
-                dataBuffer[unity_InstanceID].scale[0];
+                LOD0Buffer[unity_InstanceID].scale[0];
             unity_ObjectToWorld._12_22_32_42 = float4(0, pos.w, 0, 0) *
-                dataBuffer[unity_InstanceID].scale[1];
+                LOD0Buffer[unity_InstanceID].scale[1];
             unity_ObjectToWorld._13_23_33_43 = float4(0, 0, pos.w, 0) *
-                dataBuffer[unity_InstanceID].scale[2];
-            unity_ObjectToWorld._14_24_34_44 = float4(pos.xyz, 1);
+                LOD0Buffer[unity_InstanceID].scale[2];
+#endif
+#ifdef LOD1
+            pos = LOD1Buffer[unity_InstnceID].position;
+            unity_ObjectToWorld._11_21_31_41 = float4(pos.w, 0, 0, 0) *
+                LOD1Buffer[unity_InstanceID].scale[0];
+            unity_ObjectToWorld._12_22_32_42 = float4(0, pos.w, 0, 0) *
+                LOD1Buffer[unity_InstanceID].scale[1];
+            unity_ObjectToWorld._13_23_33_43 = float4(0, 0, pos.w, 0) *
+                LOD1Buffer[unity_InstanceID].scale[2];
+#endif
+#ifdef LOD2
+            pos = LOD2Buffer[unity_InstnceID].position;
+            unity_ObjectToWorld._11_21_31_41 = float4(pos.w, 0, 0, 0) *
+                LOD2Buffer[unity_InstanceID].scale[0];
+            unity_ObjectToWorld._12_22_32_42 = float4(0, pos.w, 0, 0) *
+                LOD2Buffer[unity_InstanceID].scale[1];
+            unity_ObjectToWorld._13_23_33_43 = float4(0, 0, pos.w, 0) *
+                LOD2Buffer[unity_InstanceID].scale[2];
+#endif
+#ifdef LOD3
+            pos = LOD3Buffer[unity_InstnceID].position;
+            unity_ObjectToWorld._11_21_31_41 = float4(pos.w, 0, 0, 0) *
+                LOD3Buffer[unity_InstanceID].scale[0];
+            unity_ObjectToWorld._12_22_32_42 = float4(0, pos.w, 0, 0) *
+                LOD3Buffer[unity_InstanceID].scale[1];
+            unity_ObjectToWorld._13_23_33_43 = float4(0, 0, pos.w, 0) *
+                LOD3Buffer[unity_InstanceID].scale[2];
+#endif
 
+            unity_ObjectToWorld._14_24_34_44 = float4(pos.xyz, 1);
             unity_ObjectToWorld = mul(modelMatrix, unity_ObjectToWorld);
 
             unity_WorldToObject = unity_ObjectToWorld;
--- Assets/_Scripts/RenderStrategies/Instanced.cs
+++ Assets/_Scripts/RenderStrategies/Instanced.cs
@@ -20,33 +20,35 @@ public class Instanced : RenderStrategy {
     private float LOD0 = 0.60f;
     private float LOD1 = 0.25f;
     private float LOD2 = 0.08f;
-    private float LOD3 = 0.1f;
+    private float LOD3 = 100;
 
-    private uint[][] _LODArgs;
+    private string[] _keywords = { "LOD0", "LOD1", "LOD2", "LOD3" };
+    private string _recentKeyword = "LOD0";
 
-    private List<Vector3>[] _LODPositions;
+    private List<uint>[] _LODArgs;
+    private List<ObjInfo>[] _LODData;
 
     private int cachedInstanceCount = -1;
     private int cachedSubMeshIndex = -1;
-    private ComputeBuffer dataBuffer;
-    private ComputeBuffer[] LODArgsBuffer;
+    private ComputeBuffer[] _LODBuffers;
+    private ComputeBuffer[] _LODArgsBuffer;
 
     public Instanced(GameObject p, GameObject o, Material mat, List<Vector3> poses, int total) : 
         base(p, o, mat, poses, total) {
-        //_LODArgs = new uint[][4];
-        LODArgsBuffer = new ComputeBuffer[4];
-        _LODPositions = new List<Vector3>[4];
+        _LODBuffers = new ComputeBuffer[4];
+        _LODArgs = new List<uint>[4];
+        _LODArgsBuffer = new ComputeBuffer[4];
+        _LODData = new List<ObjInfo>[4];
 
         for (int i = 0; i < 4; i++) {
-            _LODArgs[i] = new uint[5] { 0, 0, 0, 0, 0 };
+            _LODArgs[i] = new List<uint>(new uint[] { 0, 0, 0, 0, 0 });
         }
         _instancedObject = o;
         _objMesh = _objMeshArray[1];
 
-        for (int i = 0; i < LODArgsBuffer.Length; i++) {
-            LODArgsBuffer[i] = new ComputeBuffer(1, _LODArgs[i].Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        for (int i = 0; i < _LODArgsBuffer.Length; i++) {
+            _LODArgsBuffer[i] = new ComputeBuffer(1, _LODArgs[i].Count * sizeof(uint), ComputeBufferType.IndirectArguments);
         }
-        UpdateLOD();
         UpdateBuffers();
 
         // Set our parent model matrix
@@ -63,9 +65,8 @@ public class Instanced : RenderStrategy {
             _objMat.SetMatrix("modelMatrix", _modelMatrix);
         }
 
-        if (_cachedCamPosition != cam.transform.position) {
-            UpdateLOD();
-        }
+        if (_cachedCamPosition != cam.transform.position)
+            _cachedCamPosition = cam.transform.position;
 
         // Update starting position buffer
         if (cachedInstanceCount != TOTALOBJECTS || cachedSubMeshIndex != subMeshIndex)
@@ -74,8 +75,12 @@ public class Instanced : RenderStrategy {
         RotatePositions();
 
         // Render
-        for (int i = 0; i < LODArgsBuffer.Length; i++) {
-            Graphics.DrawMeshInstancedIndirect(_objMesh, subMeshIndex, _objMat, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), LODArgsBuffer[i]);
+        for (int i = 0; i < _LODArgsBuffer.Length; i++) {
+            _objMat.DisableKeyword(_recentKeyword);
+            _objMat.EnableKeyword(_keywords[i]);
+            _recentKeyword = _keywords[i];
+            if (_LODArgsBuffer[i] != null)
+                Graphics.DrawMeshInstancedIndirect(_objMeshArray[i], subMeshIndex, _objMat, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), _LODArgsBuffer[i]);
         }
     }
 
@@ -84,23 +89,14 @@ public class Instanced : RenderStrategy {
         if (_objMesh != null)
             subMeshIndex = Mathf.Clamp(subMeshIndex, 0, _objMesh.subMeshCount - 1);
 
-        // Data struct
-        if (dataBuffer != null)
-            dataBuffer.Release();
-
-        dataBuffer = new ComputeBuffer(TOTALOBJECTS, 12 * sizeof(float));
         ObjInfo[] data = new ObjInfo[TOTALOBJECTS];
+        for (int i = 0; i < _LODData.Length; i++) {
+            _LODData[i] = new List<ObjInfo>();
+        }
 
         for (int i = 0; i < TOTALOBJECTS; i++) {
             // Set the position of our instance
             data[i].position = new Vector4(_objPositions[i].x, _objPositions[i].y, _objPositions[i].z, 1);
-
-            // Set the color of our instance
-            float red = Random.Range(0f, 1f);
-            float green = Random.Range(0f, 1f);
-            float blue = Random.Range(0f, 1f);
-            float alpha = 1;
-            data[i].color = new Vector4(red, green, blue, alpha);
 
             // Set the scale of our instance
             float xScale = Random.Range(0.01f, 0.15f);
@@ -108,15 +104,45 @@ public class Instanced : RenderStrategy {
             float zScale = Random.Range(0.01f, 0.15f);
             float w = 1;
             data[i].scale = new Vector4(xScale, yScale, zScale, w);
+
+            float dist = Vector3.Distance(data[i].position, _cachedCamPosition);
+            float scal = data[i].scale.magnitude;
+
+            if (1 / dist * scal < 0.00967f && 1 / dist * scal > 0.001f) {
+                data[i].color = Color.white;
+                _LODData[3].Add(data[i]);
+            } else if (1 / dist * scal < 0.0295f) {
+                data[i].color = Color.green;
+                _LODData[2].Add(data[i]);
+            } else if (1 / dist * scal < 0.0689f) {
+                data[i].color = Color.blue;
+                _LODData[1].Add(data[i]);
+            } else {
+                data[i].color = Color.yellow;
+                _LODData[0].Add(data[i]);
+            }
         }
-        dataBuffer.SetData(data);
-        _objMat.SetBuffer("dataBuffer", dataBuffer);
+
+        for (int i = 0; i < _LODBuffers.Length; i++) {
+            if (_LODBuffers[i] != null) {
+                _LODBuffers[i].Release();
+            }
+            if (_LODData[i].Count > 0) {
+                _LODBuffers[i] = new ComputeBuffer(_LODData[i].Count, 12 * sizeof(float));
+                _LODBuffers[i].SetData(_LODData[i]);
+            }
+        }
+
+        _objMat.SetBuffer("LOD0Buffer", _LODBuffers[0]);
+        _objMat.SetBuffer("LOD1Buffer", _LODBuffers[1]);
+        _objMat.SetBuffer("LOD2Buffer", _LODBuffers[2]);
+        _objMat.SetBuffer("LOD3Buffer", _LODBuffers[3]);
 
         // Indirect args
         if (_objMesh != null) {
             for (int i = 0; i < _LODArgs.Length; i++) {
                 _LODArgs[i][0] = (uint)_objMesh.GetIndexCount(subMeshIndex);
-                _LODArgs[i][1] = (uint)_LODPositions[i].Count;
+                _LODArgs[i][1] = (uint)_LODData[i].Count;
                 _LODArgs[i][2] = (uint)_objMesh.GetIndexStart(subMeshIndex);
                 _LODArgs[i][3] = (uint)_objMesh.GetBaseVertex(subMeshIndex);
             }
@@ -126,33 +152,38 @@ public class Instanced : RenderStrategy {
             }
         }
 
-        for (int i = 0; i < LODArgsBuffer.Length; i++) {
-            LODArgsBuffer[i].SetData(_LODArgs[i]);
+        for (int i = 0; i < _LODArgsBuffer.Length; i++) {
+            _LODArgsBuffer[i].SetData(_LODArgs[i]);
         }
 
         cachedInstanceCount = TOTALOBJECTS;
         cachedSubMeshIndex = subMeshIndex;
     }
 
-    void UpdateLOD() {
-        Sort();
-        LevelOfDetail();
-    }
-
     void RotatePositions() {
-        ObjInfo[] data = new ObjInfo[TOTALOBJECTS];
-        dataBuffer.GetData(data);
+        _objPositions = new List<Vector3>();
+        for (int i = 0; i < _LODData.Length; i++) {
+            for (int j = 0; j < _LODData[i].Count; j++) {
+                // Set the positions of our instance based on our input List
+                float scaleMag = new Vector3(_LODData[i][j].scale.x, _LODData[i][j].scale.y, _LODData[i][j].scale.z).magnitude;
+                Vector3 newPos = Quaternion.AngleAxis(
+                    scaleMag * scaleMag * Time.deltaTime * 100,
+                    Vector3.up) * _LODData[i][j].position;
+                ObjInfo temp = _LODData[i][j];
+                temp.position = new Vector4(newPos.x, newPos.y, newPos.z, 1);
+                _LODData[i][j] = temp;
 
-        for (int i = 0; i < TOTALOBJECTS; i++) {
-            // Set the positions of our instance based on our input List
-            float scaleMag = new Vector3(data[i].scale.x, data[i].scale.y, data[i].scale.z).magnitude;
-            _objPositions[i] = Quaternion.AngleAxis(
-                scaleMag * scaleMag * Time.deltaTime * 100,
-                Vector3.up) * _objPositions[i];
-            data[i].position = new Vector4(_objPositions[i].x, _objPositions[i].y, _objPositions[i].z, 1);
+                _objPositions.Add(temp.position);
+            }
         }
-        dataBuffer.SetData(data);
-        _objMat.SetBuffer("dataBuffer", dataBuffer);
+        for (int i = 0; i < _LODBuffers.Length; i++) {
+            if (_LODBuffers[i] != null)
+                _LODBuffers[i].SetData(_LODData[i]);
+        }
+        _objMat.SetBuffer("LOD0Buffer", _LODBuffers[0]);
+        _objMat.SetBuffer("LOD1Buffer", _LODBuffers[1]);
+        _objMat.SetBuffer("LOD2Buffer", _LODBuffers[2]);
+        _objMat.SetBuffer("LOD3Buffer", _LODBuffers[3]);
     }
 
     static int SortByDistanceToCamera(Vector3 a, Vector3 b) {
@@ -163,94 +194,14 @@ public class Instanced : RenderStrategy {
         _objPositions.Sort(SortByDistanceToCamera);
     }
 
-    void LevelOfDetail() {
-        for (int i = 0; i < _LODPositions.Length; i++) {
-            _LODPositions[i].Clear();
-            _LODPositions[i] = new List<Vector3>();
-        }
-
-        for (int i = 0; i < TOTALOBJECTS; i++) {
-            Vector2 objectScreenCoord;
-            float objYMin = Screen.height;
-            float objYMax = 0;
-
-            for (int j = 0; j < 8; j++) {
-                objectScreenCoord = cam.WorldToScreenPoint(FindBoundCoords(j, _obj));
-
-                if (objectScreenCoord.y > objYMax) {
-                    objYMax = objectScreenCoord.y;
-                } else if (objectScreenCoord.y < objYMin) {
-                    objYMin = objectScreenCoord.y;
-                }
-            }
-
-            float height = objYMax - objYMin;
-
-            float relativeHeight = height / Screen.height;
-
-            if (relativeHeight > LOD0) {
-                _LODPositions[0].Add(_objPositions[i]);
-            } else if (relativeHeight > LOD1) {
-                _LODPositions[1].Add(_objPositions[i]);
-            } else if (relativeHeight > LOD2) {
-                _LODPositions[2].Add(_objPositions[i]);
-            } else {
-                _LODPositions[3].Add(_objPositions[i]);
-            }
-        }
-            
-    }
-
-    /*
-     * https://answers.unity.com/questions/352738/gameobject-width-in-screen-space.html
-     * Returns bounding box points
-     */
-    private Vector3 FindBoundCoords(int i, GameObject _gameObject) {
-        ObjInfo[] data = new ObjInfo[TOTALOBJECTS];
-        dataBuffer.GetData(data);
-
-        Vector3 scale = data[i].scale;
-        Vector3 pos = data[i].position;
-
-        // Given scale and position, find a bounding box
-        _instancedObject.transform.localScale = scale;
-        Bounds _TargetBounds = _instancedObject.GetComponentInChildren<MeshRenderer>().bounds;
-        Vector3 _TargetCenter = _TargetBounds.center + pos;
-        Vector3 _TargetExtents = _TargetBounds.extents;
-
-        switch (i) {
-            case 0:
-                return _TargetCenter + pos + new Vector3(_TargetExtents.x, _TargetExtents.y, _TargetExtents.z);
-            case 1:
-                return _TargetCenter + pos + new Vector3(_TargetExtents.x, _TargetExtents.y, _TargetExtents.z * -1);
-            case 2:
-                return _TargetCenter + pos + new Vector3(_TargetExtents.x, _TargetExtents.y * -1, _TargetExtents.z);
-            case 3:
-                return _TargetCenter + pos + new Vector3(_TargetExtents.x, _TargetExtents.y * -1, _TargetExtents.z * -1);
-            case 4:
-                return _TargetCenter + pos + new Vector3(_TargetExtents.x * -1, _TargetExtents.y, _TargetExtents.z);
-            case 5:
-                return _TargetCenter + pos + new Vector3(_TargetExtents.x * -1, _TargetExtents.y, _TargetExtents.z * -1);
-            case 6:
-                return _TargetCenter + pos + new Vector3(_TargetExtents.x * -1, _TargetExtents.y * -1, _TargetExtents.z);
-            case 7:
-                return _TargetCenter + pos + new Vector3(_TargetExtents.x * -1, _TargetExtents.y * -1, _TargetExtents.z * -1);
-            default:
-                return Vector3.zero;
-        }
-    }
-
     public override void Destroy() {
-        if (dataBuffer != null)
-            dataBuffer.Release();
-        dataBuffer = null;
-
-        for (int i = 0; i < LODArgsBuffer.Length; i++) {
-            if (LODArgsBuffer[i] != null) {
-                LODArgsBuffer[i].Release();
+        for (int i = 0; i < _LODArgsBuffer.Length; i++) {
+            if (_LODArgsBuffer[i] != null) {
+                _LODArgsBuffer[i].Release();
             }
-            LODArgsBuffer = null;
+            _LODArgsBuffer[i] = null;
         }
+        _LODArgsBuffer = null;
     }
 }
  
