e4303b71fcd302f5c96c795dcecc6eea93c6dc5e
Daniel Olson <olso7118@umn.edu>
2018-09-11 15:23:18 -0500

    Working on demo video to show off in website - hides all info on project

---
+++ Assets/Other/3rd-Party/Vertikal/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VTK;
+
+public class Test : MonoBehaviour {
+    public Text versionText;
+	// Use this for initialization
+	void Start () {
+        print("Using VTK version " + vtkVersion.GetVTKVersion());		
+	    if(versionText != null)
+            versionText.text = "Using VTK version " + vtkVersion.GetVTKVersion();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Other/_DanielRendering/MoveCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    Vector2 prevMousePos;
+    Vector2 currMousePos;
+    bool mouseDown = false;
+    int speed;
+
+    Camera cam;
+    Transform camTransform;
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (cam == null) {
+            cam = Camera.main;
+            camTransform = Camera.main.transform;
+        }
+
+        float lrAxisValue = Input.GetAxis("Horizontal");
+        float udAxisValue = Input.GetAxis("Vertical");
+
+        currMousePos = Input.mousePosition;
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            speed = 3;
+        } else {
+            speed = 1;
+        }
+
+        if (Input.GetMouseButton(1)) {
+            if (!mouseDown) {
+                mouseDown = true;
+                prevMousePos = currMousePos;
+            }
+            Vector3 diff = currMousePos - prevMousePos;
+
+            if (diff.x > 0) diff.x = 2f;
+            else if (diff.x < 0) diff.x = -2f;
+
+            if (diff.y > 0) diff.y = 2f;
+            else if (diff.y < 0) diff.y = -2f;
+
+            camTransform.Rotate(camTransform.right, -diff.y * speed, Space.World);
+            camTransform.Rotate(Vector3.up, diff.x * speed, Space.World);
+
+            prevMousePos = currMousePos;
+        } else {
+            if (mouseDown) {
+                mouseDown = false;
+            }
+        }
+        camTransform.Translate(camTransform.forward * udAxisValue * speed, Space.World);
+        camTransform.Translate(camTransform.right * lrAxisValue * speed, Space.World);
+    }
+}
